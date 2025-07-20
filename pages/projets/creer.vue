b6dfe46d819b0f9a604c0990b9838fcdfeae1791
littleBlackMan <sandyrazafitrimo@gmail.com>
2025-07-20 19:10:57 +0200

    after copilot destruction

---
+++ pages/projets/creer.vue
@@ -0,0 +1,37 @@
+<script setup>
+definePageMeta({
+  middleware: 'auth'
+})
+
+import { useAuthStore } from '~/store/auth';
+
+const auth = useAuthStore();
+auth.requireAuth();
+</script>
+
+<template>
+  <div>
+    <div class="flex" style="height: 60vh;">
+      
+      <ProjectForm 
+        :is-edit="false"
+      />
+      
+      <!-- Colonne image -->
+      <div class="w-1/2 h-screen overflow-hidden" style="height: 92vh;">
+        <img src="/images/tana.jpg"
+             class="w-full h-full object-cover"
+             alt="Tana le Kompagnon idéal">
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+}
+</style>
